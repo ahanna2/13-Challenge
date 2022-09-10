f20d3222c617ff391be1191d0a6b2d2d86ef795e
ahanna2 <103732152+ahanna2@users.noreply.github.com>
2022-09-10 17:46:33 -0500

    first push

---
+++ Develop/routes/index.js
@@ -0,0 +1,11 @@
+// Given Code
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.send("<h1> Wrong Route! Try again after fixing the route</h1>")
+});
+
+module.exports = router;
